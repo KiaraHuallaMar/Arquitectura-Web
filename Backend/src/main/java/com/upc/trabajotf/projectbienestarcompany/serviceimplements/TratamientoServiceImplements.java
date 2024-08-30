485fd03acceaaa06e1da69127ccab3f4cdb30160
aldair015 <parinoaldair@gmail.com>
2024-08-30 17:58:23 -0500

    Update TratamientoServiceImplements.java

--- Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceimplements/TratamientoServiceImplements.java
+++ Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceimplements/TratamientoServiceImplements.java
@@ -11,6 +11,28 @@ import java.util.List;
 public class TratamientoServiceImplements implements TratamientoServiceInterfaces {
     @Autowired
     private TratamientoRepository tratamientoRepository;
+
+    //Listar Tratamiento
     @Override
     public List<Tratamiento> listar() {return tratamientoRepository.findAll();}
+
+    //Registrar Tratamiento
+    @Override
+    public Tratamiento registrar(Tratamiento tratamiento) {return tratamientoRepository.save(tratamiento);}
+
+    //Listar por Id Tratamiento
+    @Override
+    public Tratamiento listarId(int id) {
+        return tratamientoRepository.findById(id).orElse(new Tratamiento());
+    }
+
+    //Actualizar Tratamiento
+    @Override
+    public void actualizar(Tratamiento tratamiento) {tratamientoRepository.save(tratamiento); }
+
+    //Eliminar Tratamiento
+    @Override
+    public void eliminar(int id) {
+        tratamientoRepository.deleteById(id);
+    }
 }

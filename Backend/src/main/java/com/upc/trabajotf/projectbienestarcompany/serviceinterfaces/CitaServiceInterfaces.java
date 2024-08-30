3dbc0a101ace8de59730d1e08126991adb8c4d4f
aldair015 <parinoaldair@gmail.com>
2024-08-30 17:58:43 -0500

    Update CitaServiceInterfaces.java

--- Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceinterfaces/CitaServiceInterfaces.java
+++ Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceinterfaces/CitaServiceInterfaces.java
@@ -7,6 +7,17 @@ import java.util.List;
 public interface CitaServiceInterfaces {
     //Listar Cita
     public List<Cita> listar();
+
     //Registrar Cita
     public Cita registrar (Cita cita);
+
+    //Listar por Id Cita
+    public Cita listarId(int id);
+
+    //Actualizar Cita
+    public void actualizar(Cita cita);
+
+    //Eliminar Cita
+    public void  eliminar(int id);
+
 }

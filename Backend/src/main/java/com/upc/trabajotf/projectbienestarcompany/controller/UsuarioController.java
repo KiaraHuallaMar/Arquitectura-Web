ea3efe58d01a50323061366a2312d2c85cfbf910
aldair015 <parinoaldair@gmail.com>
2024-08-30 17:57:38 -0500

    Update UsuarioController.java

--- Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/controller/UsuarioController.java
+++ Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/controller/UsuarioController.java
@@ -1,14 +1,11 @@
 package com.upc.trabajotf.projectbienestarcompany.controller;
 
-import com.upc.trabajotf.projectbienestarcompany.dtos.RolDTO;
 import com.upc.trabajotf.projectbienestarcompany.dtos.UsuarioDTO;
-import com.upc.trabajotf.projectbienestarcompany.serviceinterfaces.TratamientoServiceInterfaces;
+import com.upc.trabajotf.projectbienestarcompany.entities.Usuario;
 import com.upc.trabajotf.projectbienestarcompany.serviceinterfaces.UsuarioServiceInterfaces;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +17,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioServiceInterfaces usuarioServiceInterfaces;
 
+    //Listar Usuario
     @GetMapping
     public List<UsuarioDTO> obtenerUsuarios(){
 
@@ -27,5 +25,35 @@ public class UsuarioController {
             ModelMapper m=new ModelMapper();
             return m.map(x,UsuarioDTO.class);
         }).collect(Collectors.toList());
+    }
+
+    //Registrar Usuario
+    @PostMapping
+    public void registrar(@RequestBody UsuarioDTO usuarioDTO){
+        ModelMapper m = new ModelMapper();
+        Usuario usuario = m.map(usuarioDTO, Usuario.class);
+        usuarioServiceInterfaces.registrar(usuario);
+    }
+
+    //Listar por Id Usuario
+    @GetMapping("/{id}")
+    public UsuarioDTO listarId(@PathVariable("id") Integer id){
+        ModelMapper m=new ModelMapper();
+        UsuarioDTO usuarioDTO=m.map(usuarioServiceInterfaces.listarId(id),UsuarioDTO.class);
+        return usuarioDTO;
+    }
+
+    //Actualizar Usuario
+    @PutMapping
+    public void actualizar(@RequestBody UsuarioDTO usuarioDTO){
+        ModelMapper m=new ModelMapper();
+        Usuario usuario=m.map(usuarioDTO,Usuario.class);
+        usuarioServiceInterfaces.actualizar(usuario);
+    }
+
+    //Eliminar Usuario
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        usuarioServiceInterfaces.eliminar(id);
     }
 }

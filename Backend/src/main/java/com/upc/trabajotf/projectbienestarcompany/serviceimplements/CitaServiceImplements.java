31bb532a22ec41fc6d3c8dee74694483802dd030
aldair015 <parinoaldair@gmail.com>
2024-08-30 17:57:55 -0500

    Update CitaServiceImplements.java

--- Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceimplements/CitaServiceImplements.java
+++ Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceimplements/CitaServiceImplements.java
@@ -12,9 +12,28 @@ import java.util.List;
 public class CitaServiceImplements implements CitaServiceInterfaces {
     @Autowired
     private CitaRepository citaRepository;
-    @Override
+
     //Listar Cita
+    @Override
     public List<Cita> listar() {return citaRepository.findAll();}
+
     //Registrar Cita
+    @Override
     public Cita registrar(Cita cita) {return citaRepository.save(cita);}
+
+    //Listar por Id Cita
+    @Override
+    public Cita listarId(int id) {
+        return citaRepository.findById(id).orElse(new Cita());
+    }
+
+    //Actualizar Cita
+    @Override
+    public void actualizar(Cita v) {citaRepository.save(v); }
+
+    //Eliminar Cita
+    @Override
+    public void eliminar(int id) {
+        citaRepository.deleteById(id);
+    }
 }

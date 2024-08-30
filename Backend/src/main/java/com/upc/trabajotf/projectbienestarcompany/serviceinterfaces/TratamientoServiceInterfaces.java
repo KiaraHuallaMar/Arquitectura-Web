75f18d6d184b0191132fdb6d8e8bdc5b9fef6cbc
aldair015 <parinoaldair@gmail.com>
2024-08-30 17:59:05 -0500

    Update TratamientoServiceInterfaces.java

--- Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceinterfaces/TratamientoServiceInterfaces.java
+++ Backend/src/main/java/com/upc/trabajotf/projectbienestarcompany/serviceinterfaces/TratamientoServiceInterfaces.java
@@ -5,5 +5,19 @@ import com.upc.trabajotf.projectbienestarcompany.entities.Tratamiento;
 import java.util.List;
 
 public interface TratamientoServiceInterfaces {
+
+    //Listar Tratamiento
     public List<Tratamiento> listar();
+
+    //Registrar Tratamiento
+    public Tratamiento registrar (Tratamiento tratamiento);
+
+    //Listar por Id Tratamiento
+    public Tratamiento listarId(int id);
+
+    //Actualizar Tratamiento
+    public void actualizar(Tratamiento tratamiento);
+
+    //Eliminar Tratamiento
+    public void  eliminar(int id);
 }
